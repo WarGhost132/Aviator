03ba35712b91d1243d2436652a955f4c8efb9a4d
WarGhost132 <wawghostick@gmail.com>
2024-08-29 13:39:58 +0300

    Add code

---
+++ pages/white/index.php
@@ -0,0 +1,195 @@
+<?php
+
+// Head
+$PAGE_TITLE = "AVIATOR";
+$PAGE_DESCRIPTION = "Description";
+
+// Content below
+?>
+
+<main>
+    <section class="section main__section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 d-none d-md-block position-relative px-0">
+                    <div data-aos="fade-up" class="main__image__wrapper d-flex">
+                        <img data-aos="fade-up" data-aos-delay="300" src="assets/images/main.webp" alt="">
+                    </div>
+                    <div class="circle__main__wrapper position-absolute">
+                        <div class="main__circle d-flex justify-content-center align-items-center">
+                            <div class="child__main__circle"></div>
+                        </div>
+                    </div>
+                    <div class="circle__main__white__wrapper position-absolute">
+                        <div class="white__circle d-flex justify-content-center align-items-center"></div>
+                    </div>
+                    <div class="circle__main__red__wrapper position-absolute">
+                        <div class="red__circle d-flex justify-content-center align-items-center"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 pe-0">
+                    <div class="main__content">
+                        <div data-aos="zoom-in-up" class="main__title__wrapper d-flex justify-content-start justify-content-md-end align-items-center">
+                            <h1 class="pt-3">Aviator</h1>
+                        </div>
+                        <div class="main__content__wrapper pt-4 pt-md-5 d-flex flex-column align-items-start align-items-md-end text-start text-md-end">
+                            <p data-aos="fade-left" class="subtitle">Ощутите Взлёт Азарта!</p>
+                            <p data-aos="fade-up-left">Добро пожаловать в Aviator, игру, которая перенесет вас на крыльях в мир адреналина и азарта.</p>
+                            <br>
+                            <p data-aos="fade-up-left">Здесь каждое мгновение захватывающее и наполнено ожиданием больших выигрышей! Приготовьтесь отправиться в захватывающее путешествие, где небо — это не предел.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section advantages__section position-relative pt-0">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 px-0">
+                    <div class="advantages__title__wrapper gradient__wrapper py-3">
+                        <div class="row">
+                            <div class="col-12 col-lg-6">
+                                <h2 data-aos="fade-up" class="ps-5">Четыре причины выбрать Aviator для вашего игрового приключения:</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-center py-1 py-md-5">
+                        <div class="secondary__wrapper w-100 left d-flex justify-content-start">
+                            <div class="advantages__content text-start text-md-center">
+                                <div class="content">
+                                    <p data-aos="fade-down" class="subtitle text-center">Возвышающие выигрыши ждут вас</p>
+                                    <p data-aos="fade-up">Каждый полет — это шанс взлететь на новые высоты и сорвать невероятный куш!</p>
+                                    <br>
+                                    <p data-aos="fade-up">Aviator предлагает уникальный игровой опыт, в котором вы сами решаете, когда забрать свой выигрыш, добавляя элемент стратегии к вашему азарту.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__image__wrapper right__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="first" src="assets/images/advantages_1.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-center py-1 py-md-5 helper__wrapper">
+                        <div class="w-100 d-flex justify-content-start helper__wrapper">
+                            <div class="w-100 left d-flex justify-content-start">
+                                <div class="advantages__content text-start text-md-center py-3">
+                                    <div class="content">
+                                        <p data-aos="fade-down" class="subtitle text-center">Бесконечное волнение</p>
+                                        <p data-aos="fade-up">Почувствуйте настоящий адреналин с каждым взлетом! </p>
+                                        <br>
+                                        <p data-aos="fade-up">Aviator — это игра для тех, кто не боится риска. Она постоянно держит вас на грани, предлагая захватывающий и динамичный геймплей.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__image__wrapper right__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="second" src="assets/images/advantages_2.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-center py-1 py-md-5">
+                        <div class="secondary__wrapper w-100 d-flex right justify-content-end">
+                            <div class="advantages__content text-start text-md-center">
+                                <div class="content">
+                                    <p data-aos="fade-down" class="subtitle text-center">Захватывающие визуальные эффекты</p>
+                                    <p data-aos="fade-up">Погрузитесь в атмосферу полета с помощью захватывающей графики и анимации.</p>
+                                    <br>
+                                    <p data-aos="fade-up">Каждое действие в игре сопровождается эффектными визуальными элементами, создающими эффект полного погружения.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__image__wrapper left__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="third" src="assets/images/advantages_3.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-center py-1 py-md-5">
+                        <div class="w-100 d-flex justify-content-end right">
+                            <div class="advantages__content text-start text-md-center pt-3">
+                                <div class="content">
+                                    <p data-aos="fade-down" class="subtitle text-center">Доступно для всех</p>
+                                    <p data-aos="fade-up">Независимо от вашего опыта в играх, Aviator предлагает простой и интуитивно понятный интерфейс.</p>
+                                    <br>
+                                    <p data-aos="fade-up">Это идеальная игра как для новичков, так и для опытных игроков, желающих испытать что-то новое и увлекательное.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Aviator image -->
+            <div class="aviator-advantages-image-wrapper position-absolute">
+                <div class="position-relative">
+                    <!-- Advantages-1 -->
+                    <div class="position-absolute advantages-1-wrapper">
+                        <div data-aos="fade-down" class="advantages-1-image"></div>
+                    </div>
+                    <!-- Advantages-2 -->
+                    <div class="position-absolute advantages-2-wrapper">
+                        <div data-aos="fade-up" class="advantages-2-image"></div>
+                    </div>
+                    <!-- Advantages-3 -->
+                    <div class="position-absolute advantages-3-wrapper">
+                        <div data-aos="fade-right" class="advantages-3-image position-relative">
+                            <div class="circle__advantages__wrapper position-absolute">
+                                <div class="advantages__circle d-flex justify-content-center align-items-center">
+                                    <div class="child__advantages__circle"></div>
+                                </div>
+                            </div>
+                            <div class="circle__advantages__white__wrapper position-absolute">
+                                <div class="white__circle d-flex justify-content-center align-items-center"></div>
+                            </div>
+                            <div class="circle__advantages__red__wrapper position-absolute">
+                                <div class="red__circle d-flex justify-content-center align-items-center"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="overflow-hidden">
+                        <!-- Back part 1 -->
+                        <div class="position-absolute back-part-1-wrapper">
+                            <div class="back-part-1-image"></div>
+                        </div>
+                    </div>
+                    <!-- Back part 2 -->
+                    <div class="position-absolute back-part-2-wrapper">
+                        <div class="back-part-2-image"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section games__section pt-0">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center py-5">
+                    <button data-aos="fade-right" class="px-4 py-2">Попробовать сейчас<span class="flare"></span></button>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
